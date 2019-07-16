e2cdcd01944cd09e75c7acce6a432e8cd0b594b5
tbaums <michael.a.tanenbaum@gmail.com>
2019-07-16 11:46:51 -0400

    working flow to FE for multiple actors

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,16 +1,13 @@
+<!-- TODO: Move all this to separate component -->
 <style>
-	h1,
-	figure,
-	p {
-		text-align: center;
-		margin: 0 auto;
-	}
+	/* TODO: Implement CSS grid */
 
 	h1 {
 		font-size: 2.8em;
 		text-transform: uppercase;
 		font-weight: 700;
 		margin: 0 0 0.5em 0;
+		text-align: center;
 	}
 
 	.actor {
@@ -25,12 +22,15 @@
 	#map {
 		width: 60%;
 		height: 30em;
-		border: 1px solid darkblue;
+		border: 3px solid darkblue;
 		position: relative;
 	}
 
-	#map > p {
+	#map>p {
 		margin: 0;
+		border: 0;
+		padding: 0;
+		line-height: 0;
 	}
 
 	p {
@@ -50,23 +50,39 @@
 
 
 <script>
+	import { actors, map } from './stores.js'
+	let x, y
+	$: actor_list = gen_actor_list()
+
+
 	let messages = ''
 	let data = []
+
 	let prev_messages = ''
 	let new_messages = ''
-	let x = 0
-	let y = 0
+
 
 	// Prevennts an error when Svelte does SSR.
 	if (process.browser) {
 		let xhr = new XMLHttpRequest()
 	}
 
-	async function handleClick(event) {
+	//need to create an array of actors because Svelte can't iterate over Map objects 
+	function gen_actor_list() {
+		let arr = [];
+		for (let item of $map.keys()) {
+			arr.push(item)
+		}
+		return arr
+	}
+
+
+
+	async function fetch_map(event) {
 		let xhr = new XMLHttpRequest()
 		xhr.responseType = 'text';
 		// Change this to ENV variable with const x = process.env.X;
-		xhr.open("GET", 'https://ad51b5465a4a011e987c20aaf9c2019a-392664409.us-west-2.elb.amazonaws.com/kafka-client-api/read?topic=topic2-2')
+		xhr.open("GET", 'https://aa88ee6c922d84faba50b571b841e45d-593314138.us-west-2.elb.amazonaws.com/kafka-client-api/read?topic=actors')
 		xhr.timeout = Infinity;
 		xhr.onloadstart = function () {
 			console.log("Download underway");
@@ -88,14 +104,14 @@
 			let data_point_array = data.slice(-1)
 
 			//handle multiple data points sent in a single update
-			if (data_point_array[data_point_array.length -1].length > 1) {
+			if (data_point_array[data_point_array.length - 1].length > 1) {
 				data_point_array = data.slice(-1).pop()
 				console.log('data_point_array')
 				console.log(data_point_array)
 			} else {
 				data_point_array = data.slice(-1)
 			}
-			
+
 			let data_point = data_point_array[data_point_array.length - 1]
 			console.log('data_point: ')
 			console.log(data_point)
@@ -103,14 +119,28 @@
 
 			console.log(data_point)
 
+			let actor = data_point.actor 
 			x = data_point.x
 			y = data_point.y
 			console.log(x)
 			console.log(y)
+			console.log(actor)
+
+			$map.set(actor, { 'x': x, 'y': y })
+
+			//call gen_actor_list to update list of actors
+			//TODO: confirm if this step is necessary
+			actor_list = gen_actor_list()
+			console.log(actor_list)
+			console.log(gen_actor_list())
+			console.log($map.get(actor))
+
 		};
 		xhr.send()
 		xhr.onload = function () {
+			//TODO: reconnect on completion
 			console.log(xhr.response)
+			
 		}
 	}
 
@@ -125,13 +155,14 @@
 
 <div id="hero">
 	<div id="map">
-		<p class='actor' style="left: {x}%; bottom: {y}%;">A</p>
+		{#each actor_list as actor}
+			<p class='actor' id={actor} style="left: {$map.get(actor)['x']}%; bottom: {$map.get(actor)['y']}%;">{actor}</p>
+		{/each}
 	</div>
 </div>
 
 
-<button on:click={handleClick}>
+<button on:click={fetch_map}>
 	Click me to start fetch
 </button>
 
-<p>{data}</p>
--- src/routes/stores.js
+++ src/routes/stores.js
@@ -1,3 +1,5 @@
 import { writable } from 'svelte/store';
 
-export const kafka_data = writable([1,2,3,4])
+export const actors = writable([]);
+export const map = writable(new Map());
+
