dd856b6853eb416c8174f4c44b97c386e141f01f
tbaums <michael.a.tanenbaum@gmail.com>
2019-07-31 16:19:27 -0400

    working kafka stream to fe over websockets!

--- kafka-node-js-api/index.js
+++ kafka-node-js-api/index.js
@@ -1,5 +1,18 @@
 var WebSocketServer = require('websocket').server;
 var http = require('http');
+var kafka = require('kafka-node'),
+    Consumer = kafka.Consumer,
+    client = new kafka.KafkaClient({ kafkaHost: 'kafka-kafka-0.kafka-svc.default.svc.cluster.local:9093' }),
+    consumer = new Consumer(
+        client, [
+            { topic: 'research', partition: 0 }
+        ], {
+            autoCommit: true
+        }
+    );
+
+console.log(consumer)
+
 
 var server = http.createServer(function(request, response) {
     // process HTTP request. Since we're writing just WebSockets
@@ -18,6 +31,10 @@ wsServer = new WebSocketServer({
 wsServer.on('request', function(request) {
     var connection = request.accept(null, request.origin)
     connection.sendUTF("pongo")
+    consumer.on('message', function(message) {
+        console.log(message);
+        connection.sendUTF(message)
+    });
 
     // This is the most important callback for us, we'll handle
     // all messages from users here.
