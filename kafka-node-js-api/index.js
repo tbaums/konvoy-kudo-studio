6edb48cc30c77596a6fa22ab70caa5fa385b4b79
tbaums <michael.a.tanenbaum@gmail.com>
2019-07-31 16:01:05 -0400

    clean up

--- kafka-node-js-api/index.js
+++ kafka-node-js-api/index.js
@@ -1,31 +1,3 @@
-// var app = require('express')();
-// var http = require('http').createServer(app);
-// var io = require('socket.io')(http);
-
-// const port = 3002
-
-
-// app.get('/kafka-node-js-api', function(req, res) {
-//     res.send('<h1>Hello world</h1>');
-// });
-
-
-// io.on('connection', function(socket) {
-//     console.log('user connected');
-
-//     socket.on('disconnect', function() {
-//         console.log('user disconnected');
-//     });
-
-//     socket.on('add-message', (message) => {
-//         io.emit('message', { type: 'new-message', text: message });
-//     });
-// });
-
-// http.listen(port, function() {
-//     console.log('listening on port ' + port);
-// });
-
 var WebSocketServer = require('websocket').server;
 var http = require('http');
 
