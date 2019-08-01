357c5223c2cefd79b048f8cead32785566db6018
tbaums <michael.a.tanenbaum@gmail.com>
2019-08-01 17:47:16 -0400

    update crds

--- install-kudo-prereqs.sh
+++ install-kudo-prereqs.sh
@@ -1,13 +1,6 @@
-    
 kubectl create -f https://raw.githubusercontent.com/kudobuilder/kudo/v0.4.0/docs/deployment/00-prereqs.yaml
-sleep 1
-
 kubectl create -f https://raw.githubusercontent.com/kudobuilder/kudo/v0.4.0/docs/deployment/10-crds.yaml
-
-sleep 1
-
 kubectl create -f https://raw.githubusercontent.com/kudobuilder/kudo/v0.4.0/docs/deployment/20-deployment.yaml
-
 sleep 1
 
 kubectl create -f https://raw.githubusercontent.com/kudobuilder/operators/master/repository/kafka/docs/v0.2/resources/service-monitor.yaml
