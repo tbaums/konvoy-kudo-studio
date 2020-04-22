43604770363dbb40f2a3a858bc9f12ff71e1fc59
tbaums <michael.a.tanenbaum@gmail.com>
2020-04-21 21:39:29 -0400

    trim old files

--- install-kudo-prereqs.sh
+++
@@ -1,6 +0,0 @@
-kubectl create -f https://raw.githubusercontent.com/kudobuilder/kudo/v0.5.0/docs/deployment/00-prereqs.yaml
-kubectl create -f https://raw.githubusercontent.com/kudobuilder/kudo/v0.5.0/docs/deployment/10-crds.yaml
-kubectl create -f https://raw.githubusercontent.com/kudobuilder/kudo/842c7f19a0a361751f0dab330faf3be147c9c4b3/docs/deployment/20-deployment.yaml
-sleep 1
-
-kubectl create -f https://raw.githubusercontent.com/kudobuilder/operators/master/repository/kafka/docs/v0.2/resources/service-monitor.yaml
